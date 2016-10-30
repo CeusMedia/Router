c4c920078ebcd04123ce543501a8da1c2257d923
Christian Würker <kriss@ceusmedia.de>
2016-10-30 22:15:29 +0100

    Add router method getRoutesByController.

--- src/Router.php
+++ src/Router.php
@@ -41,37 +41,89 @@ class Router{
 
 	protected $options	= array();
 
+	/**
+	 *	Constructor.
+	 *	@access		public
+	 *	@param		array		$options	Map of options
+	 *	@return		void
+	 */
 	public function __construct( $options = array() ){
 		$this->options	= array_merge( $this->options, $options );
 		$this->registry	= new Registry();
 		$this->resolver	= new Resolver( $this->registry );
 	}
 
+	/**
+	 *	...
+	 *	@access		public
+	 *	@param		string		$controller		Name of controller class
+	 *	@param		string		$action			Name of action name
+	 *	@param		string		$pattern		Pattern to resolve route by
+	 *	@param		string		$method			HTTP method (GET|POST|PUT|DELETE)
+	 *	@return		string		ID of added route
+	 */
 	public function add( $controller, $action = 'index', $pattern, $method = '*' ){
 		$route	= new  Route( $controller, $action, $pattern, strtoupper( $method ) );
 		return $this->registry->add( $route );
 	}
 
+	/**
+	 *	...
+	 *	@access		public
+	 *	@param		Route		$route			Route instance to add
+	 *	@return		string		ID of added route
+	 */
 	public function addRoute( Route $route ){
 		return $this->registry->add( $route );
 	}
 
+	/**
+	 *	...
+	 *	@access		public
+	 *	@return		array 		List of route instances
+	 */
 	public function getRoutes(){
 		return $this->registry->index();
 	}
 
+	/**
+	 *	...
+	 *	@access		public
+	 *	@param		string		$controller		...
+	 *	@return		array 		List of route instances
+	 */
 	public function getRoutesByController( $controller ){
 		return $this->registry->indexByController( $controller );
 	}
 
+	/**
+	 *	...
+	 *	@access		public
+	 *	@param		string		$filePath		Path to routes file
+	 *	@return		void
+	 */
 	public function loadRoutesFromJsonFile( $filePath ){
 		$this->registry->loadFromJsonFile( $filePath );
 	}
 
+	/**
+	 *	...
+	 *	@access		public
+	 *	@param		string		$path			...
+	 *	@param		string		$method			HTTP method (GET|POST|PUT|DELETE)
+	 *	@param		boolean		$strict			Flag: resolve in strict mode
+	 *	@return		Route
+	 */
 	public function resolve( $path, $method = "GET", $strict = TRUE ){
-		return $this->resolver->resolve( $path, $method );
+		return $this->resolver->resolve( $path, $method, $strict );
 	}
 
+	/**
+	 *	...
+	 *	@access		public
+	 *	@param		string		$filePath		Path to routes file
+	 *	@return		void
+	 */
 	public function saveRoutes( $filePath ){
 		$this->registry->save( $filePath );
 	}

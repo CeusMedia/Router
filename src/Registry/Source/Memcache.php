1c10853b822552ce55b49932f91bad05e8fdb7e2
kriss0r <kriss@ceusmedia.de>
2024-10-24 21:53:46 +0200

    Update copyright year.

--- src/Registry/Source/Memcache.php
+++ src/Registry/Source/Memcache.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /**
  *	...
  *
- *	Copyright (c) 2016-2023 Christian Würker (ceusmedia.de)
+ *	Copyright (c) 2016-2024 Christian Würker (ceusmedia.de)
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@ declare(strict_types=1);
  *	@category		Library
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
  *	@package		CeusMedia_Router_Registry_Source
- *	@copyright		2016-2023 Christian Würker
+ *	@copyright		2016-2024 Christian Würker
  *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/Router
  */
@@ -38,7 +38,7 @@ use InvalidArgumentException;
  *	@category		Library
  *	@package		CeusMedia_Router_Registry_Source
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2016-2023 Christian Würker
+ *	@copyright		2016-2024 Christian Würker
  *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/Router
  */
